bd3a18374de3fe333523fd88d727ba0bc63c7a43
Erhan Mutlu <erhan.mutlu@iyzico.com>
2019-08-04 23:59:26 +0300

    outbound gateway

--- src/main/java/org/erhanmutlu/iyzicospringintegrationdemo/config/ProducerIntegrationConfiguration.java
+++ src/main/java/org/erhanmutlu/iyzicospringintegrationdemo/config/ProducerIntegrationConfiguration.java
@@ -1,13 +1,16 @@
 package org.erhanmutlu.iyzicospringintegrationdemo.config;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.jms.JmsOutboundGateway;
 import org.springframework.integration.jms.JmsSendingMessageHandler;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
@@ -24,12 +27,24 @@ public class ProducerIntegrationConfiguration {
         return MessageChannels.queue("midChannel").get();
     }
 
+//    @Bean
+//    @ServiceActivator(inputChannel = "midChannel")
+//    public MessageHandler jmsMessageHandler(JmsTemplate jmsTemplate) {
+//        JmsSendingMessageHandler handler = new JmsSendingMessageHandler(jmsTemplate);
+//        handler.setDestinationName("midMQChannel");
+//        return handler;
+//    }
+
     @Bean
-    @ServiceActivator(inputChannel = "midChannel")
-    public MessageHandler jmsMessageHandler(JmsTemplate jmsTemplate) {
-        JmsSendingMessageHandler handler = new JmsSendingMessageHandler(jmsTemplate);
-        handler.setDestinationName("midMQChannel");
-        return handler;
+    @ServiceActivator(inputChannel="midChannel")
+    public JmsOutboundGateway jmsGateway(ActiveMQConnectionFactory producerConnectionFactory, MessageConverter messageConverter) {
+        JmsOutboundGateway gw = new JmsOutboundGateway();
+        gw.setConnectionFactory(producerConnectionFactory);
+        gw.setRequestDestinationName("midMQChannel");
+        gw.setMessageConverter(messageConverter);
+//        gw.setReplyDestinationName("test.in");
+        gw.setCorrelationKey("JMSCorrelationID");
+        return gw;
     }
 
     @Bean
